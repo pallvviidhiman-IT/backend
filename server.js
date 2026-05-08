a65335e9bcee9cdd36f303444a5165eb3a134f44
pallvi <pallvidhimanpd786@gmail.com>
2026-05-08 22:04:02 +0530

    again fixed

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
+
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
+const User = require("./models/User");
 
 app.use(cors());           // Allow frontend connections
 app.use(express.json());   // Parse JSON request bodies
-require("dotenv").config();
 
-mongoose.connect(process.env.mongodb+srv://pallvviidhiman_db_user:<db_password>@cluster0.yxfx8pn.mongodb.net/?appName=Cluster0)
+
+mongoose.connect(process.env.MONGO_URI);
 // mongoose
 //  .connect("mongodb://127.0.0.1:27017/mernusers")
 //  .then(() => console.log("MongoDB Connected"));
 
-const User = require("./models/User");
 
-app.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.json(users);
-});
+// app.get("/users", async (req, res) => {
+//   const users = await User.find();
+//   res.json(users);
+// });
 
-app.post("/users", async (req, res) => {
-  const newUser = new User({
-    name: req.body.name
-  });
-  await newUser.save();
-  res.json(newUser);
-});
+// app.post("/users", async (req, res) => {
+//   const newUser = new User({
+//     name: req.body.name
+//   });
+//   await newUser.save();
+//   res.json(newUser);
+// });
+const cors = require("cors");
 
-app.listen(5000, () => {
-  console.log("Server Running on port 5000");
+app.use(cors({
+  origin: "*"
+}));
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running`);
 });
